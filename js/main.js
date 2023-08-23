f82e8abbc279829d9f9bd239459f94c7dd0e9a10
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-08-23 17:47:59 +0200

    feat: fix js - end exercise

--- js/main.js
+++ js/main.js
@@ -32,25 +32,10 @@ console.log(tomorrowMilliseconds);
 let countdown = tomorrowMilliseconds - presentMilliseconds;
 console.log(countdown);
 
-/* function timeConverter(milliseconds) {
-  let seconds = Math.floor(milliseconds / 1000);
-  let minutes = Math.floor(seconds / 60);
-  let hours = Math.floor(minutes / 60);
-
-  seconds = seconds % 60;
-  minutes = minutes % 60;
-  hours = hours % 24;
-} */
-
-/* function timeConverter(countdown) {
-  (d = Math.floor(countdown / 86400000)),
-    (h = Math.floor((countdown % 86400000) / 3600000)),
-    (m = Math.floor((countdown % 3600000) / 60000)),
-    (s = Math.floor((countdown % 60000) / 1000));
-  return d + "Day(s) " + h + "Hour(s) " + m + "Minute(s) " + s + "Second(s)";
-} */
 //! Trovati i dati..ma adesso? come scalare ?
-function timeConverter(countdown) {
+
+const timeRemain = setInterval(function () {
+  countdown = countdown - 1000;
   let milliseconds = Math.floor((countdown % 1000) / 100),
     seconds = Math.floor((countdown / 1000) % 60),
     minutes = Math.floor((countdown / (1000 * 60)) % 60),
@@ -60,11 +45,12 @@ function timeConverter(countdown) {
   minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
 
-  return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
-}
-console.log(timeConverter(countdown));
-
-countdown = setInterval(function () {
-  milliseconds--;
+  document.getElementById("seconds").innerHTML = seconds;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("hours").innerHTML = hours;
+  if (countdown == 0) {
+    clearTimeout(timeRemain);
+  }
 }, 1000);
-clearTimeout(countdown);
+/* let resultsTimer = timeConverter(countdown);
+  console.log(resultsTimer); */
